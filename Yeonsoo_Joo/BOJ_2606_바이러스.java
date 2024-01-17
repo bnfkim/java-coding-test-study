8e97f4d3d045b996a95cfc8f36b29b0bf0798857
jooys130 <wndustn130@gmail.com>
2024-01-17 22:20:12 +0900

    [BOJ]2606/실버3/132ms

---
+++ Yeonsoo_Joo/BOJ_2606_바이러스.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int v;
+    static int[][] graph;
+    static boolean[] visited;
+    static int count;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        v = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        graph = new int[v+1][v+1];
+        visited = new boolean[v+1];
+
+        for (int i = 0; i < e; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a][b] = graph[b][a] = 1;
+        }
+
+        dfs(1);
+
+        System.out.println(count);
+
+    }
+
+    private static void dfs(int node) {
+        visited[node] = true;
+
+        for (int i = 1; i <= v; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+    }
+}
