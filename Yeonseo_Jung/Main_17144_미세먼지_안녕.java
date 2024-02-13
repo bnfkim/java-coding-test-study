2017818f0201f5df160e571a07b67bb7c0472c3f
yeondori <yeonseo.jung0@gmail.com>
2024-02-13 23:08:11 +0900

    [BOJ]17144/골드4/472ms/2hr/정연서

--- Yeonseo_Jung/Main_17144_미세먼지_안녕.java
+++ Yeonseo_Jung/Main_17144_미세먼지_안녕.java
@@ -18,8 +18,8 @@ public class Main_17144_미세먼지_안녕 {
     }
 
     static int R, C, T;
-    static int[][] map;
-    static int[][] spread_map;
+    static int[][] A;
+    static int[][] spreadA;
     static int[] cleaner = new int[2];
     static List<Dust> dustList = new ArrayList<>();
 
@@ -34,18 +34,18 @@ public class Main_17144_미세먼지_안녕 {
         C = Integer.parseInt(st.nextToken());
         T = Integer.parseInt(st.nextToken());
 
-        map = new int[R][C];
-        spread_map = new int[R][C];
+        A = new int[R][C];
+        spreadA = new int[R][C];
 
         int cleanerIndex = 0;
         for (int i = 0; i < R; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < C; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == -1) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+                if (A[i][j] == -1) {
                     cleaner[cleanerIndex++] = i;
-                } else if (map[i][j] != 0) {
-                    dustList.add(new Dust(i, j, map[i][j]));
+                } else if (A[i][j] != 0) {
+                    dustList.add(new Dust(i, j, A[i][j]));
                 }
             }
         }
@@ -56,8 +56,8 @@ public class Main_17144_미세먼지_안녕 {
             dustList.clear();
             for (int i = 0; i < R; i++) {
                 for (int j = 0; j < C; j++) {
-                    if (map[i][j] > 0) {
-                        dustList.add(new Dust(i, j, map[i][j]));
+                    if (A[i][j] > 0) {
+                        dustList.add(new Dust(i, j, A[i][j]));
                     }
                 }
             }
@@ -80,19 +80,19 @@ public class Main_17144_미세먼지_안녕 {
                 int nx = dust.x + dx[i];
                 int ny = dust.y + dy[i];
 
-                if (nx >= 0 && nx < R && ny >= 0 && ny < C && map[nx][ny] != -1) {
-                    spread_map[nx][ny] += spreadAmount;
+                if (nx >= 0 && nx < R && ny >= 0 && ny < C && A[nx][ny] != -1) {
+                    spreadA[nx][ny] += spreadAmount;
                     spreadCount++;
                 }
             }
 
-            map[dust.x][dust.y] -= spreadAmount * spreadCount;
+            A[dust.x][dust.y] -= spreadAmount * spreadCount;
         }
 
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                map[i][j] += spread_map[i][j];
-                spread_map[i][j] = 0;
+                A[i][j] += spreadA[i][j];
+                spreadA[i][j] = 0;
             }
         }
     }
@@ -102,31 +102,31 @@ public class Main_17144_미세먼지_안녕 {
         int bottom = cleaner[1];
 
         for (int i = top - 1; i > 0; i--) {
-            map[i][0] = map[i - 1][0];
+            A[i][0] = A[i - 1][0];
         }
         for (int i = 0; i < C - 1; i++) {
-            map[0][i] = map[0][i + 1];
+            A[0][i] = A[0][i + 1];
         }
         for (int i = 0; i < top; i++) {
-            map[i][C - 1] = map[i + 1][C - 1];
+            A[i][C - 1] = A[i + 1][C - 1];
         }
         for (int i = C - 1; i > 1; i--) {
-            map[top][i] = map[top][i - 1];
+            A[top][i] = A[top][i - 1];
         }
-        map[top][1] = 0;
+        A[top][1] = 0;
 
         for (int i = bottom + 1; i < R - 1; i++) {
-            map[i][0] = map[i + 1][0];
+            A[i][0] = A[i + 1][0];
         }
         for (int i = 0; i < C - 1; i++) {
-            map[R - 1][i] = map[R - 1][i + 1];
+            A[R - 1][i] = A[R - 1][i + 1];
         }
         for (int i = R - 1; i > bottom; i--) {
-            map[i][C - 1] = map[i - 1][C - 1];
+            A[i][C - 1] = A[i - 1][C - 1];
         }
         for (int i = C - 1; i > 1; i--) {
-            map[bottom][i] = map[bottom][i - 1];
+            A[bottom][i] = A[bottom][i - 1];
         }
-        map[bottom][1] = 0;
+        A[bottom][1] = 0;
     }
 }
