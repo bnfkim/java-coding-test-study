6fb65700000558a866bf97a7622a44760ada8247
bnfkim <kgr3819@gmail.com>
2024-03-03 19:05:54 +0900

    [BOJ]31235/골드4/40m/2020ms/김규리

---
+++ Gyuri_Kim/BOJ_31236_올라올라.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/**
+ * 320552 kb
+ * 2020 ms
+ */
+public class Main_31235_올라올라 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0; i<N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int cnt = 1;
+        int answer = 1;
+        int now = arr[0];
+
+        for (int idx=1; idx<N; idx++) {
+            int next = arr[idx];
+
+            if(now > next) { //감소할 경우
+                cnt++;
+            } else  {
+                answer = Math.max(cnt, answer);
+                cnt = 1;
+                now = next;
+            }
+        }
+
+        System.out.println(Math.max(cnt, answer));
+    }
+}
