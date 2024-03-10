f7ec13f37a183fbb438bb1b8dbf2d53392c4f4a3
yeondori <yeonseo.jung0@gmail.com>
2024-03-10 16:16:26 +0900

    [BOJ]23796/골드5/84ms/1hr/정연서

--- Yeonseo_Jung/BOJ_23796_2147483648_게임.java
+++ Yeonseo_Jung/BOJ_23796_2147483648_게임.java
@@ -26,33 +26,33 @@ public class BOJ_23796_2147483648_게임 {
 
     private static void getNextBoard(String command) {
         switch (command) {
-            case "L" :
+            case "L":
                 moveLeft();
                 break;
-            case "R" :
+            case "R":
                 moveRight();
                 break;
-            case "U" :
+            case "U":
                 moveUp();
                 break;
-            case "D" :
+            case "D":
                 moveDown();
                 break;
         }
     }
 
     private static void moveDown() {
-        for (int i = BOARD_SIZE-1; i >= 0; i--) {
-            int idx = BOARD_SIZE-1;
+        for (int i = BOARD_SIZE - 1; i >= 0; i--) {
+            int idx = BOARD_SIZE - 1;
             long prev = 0;
-            for (int j = BOARD_SIZE-1; j >= 0; j--) {
+            for (int j = BOARD_SIZE - 1; j >= 0; j--) {
                 if (board[j][i] != 0) {
                     if (prev == 0) {
                         prev = board[j][i];
                         nextBoard[idx--][i] = prev;
                     } else if (prev == board[j][i]) {
                         prev = prev * 2;
-                        nextBoard[idx+1][i] = prev;
+                        nextBoard[idx + 1][i] = prev;
                         prev += 1;  // 한 타일이 두 번 이상 합쳐지는 것 방지
                     } else {
                         prev = board[j][i];
@@ -74,7 +74,7 @@ public class BOJ_23796_2147483648_게임 {
                         nextBoard[idx++][i] = prev;
                     } else if (prev == board[j][i]) {
                         prev = prev * 2;
-                        nextBoard[idx-1][i] = prev;
+                        nextBoard[idx - 1][i] = prev;
                         prev += 1; // 한 타일이 두 번 이상 합쳐지는 것 방지
                     } else {
                         prev = board[j][i];
@@ -96,7 +96,7 @@ public class BOJ_23796_2147483648_게임 {
                         nextBoard[i][idx++] = prev;
                     } else if (prev == board[i][j]) {
                         prev = prev * 2;
-                        nextBoard[i][idx-1] = prev;
+                        nextBoard[i][idx - 1] = prev;
                         prev += 1; // 한 타일이 두 번 이상 합쳐지는 것 방지
                     } else {
                         prev = board[i][j];
@@ -109,16 +109,16 @@ public class BOJ_23796_2147483648_게임 {
 
     private static void moveRight() {
         for (int i = 0; i < BOARD_SIZE; i++) {
-            int idx = BOARD_SIZE-1;
+            int idx = BOARD_SIZE - 1;
             long prev = 0;
-            for (int j = BOARD_SIZE-1; j >= 0; j--) {
+            for (int j = BOARD_SIZE - 1; j >= 0; j--) {
                 if (board[i][j] != 0) {
                     if (prev == 0) {
                         prev = board[i][j];
                         nextBoard[i][idx--] = prev;
                     } else if (prev == board[i][j]) {
                         prev = prev * 2;
-                        nextBoard[i][idx+1] = prev;
+                        nextBoard[i][idx + 1] = prev;
                         prev += 1; // 한 타일이 두 번 이상 합쳐지는 것 방지
                     } else {
                         prev = board[i][j];
@@ -128,8 +128,6 @@ public class BOJ_23796_2147483648_게임 {
             }
         }
     }
-
-
 
     private static void printBoard(long board[][]) {
         StringBuilder contents = new StringBuilder();
